05b95dc42b577f5110d7a027dc886d061ed438af
vinitshandilya <vinitshandilya@outlook.com>
2022-04-20 11:50:15 +0530

    Apple Homekit demo using HAP protocol

---
+++ simplest_homekit_demo.ino
@@ -0,0 +1,98 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+
+#include <arduino_homekit_server.h>
+
+#define PL(s) Serial.println(s)
+#define P(s) Serial.print(s)
+
+//D0 16 //led
+//D3  0 //flash button
+//D4  2 //led
+
+#define PIN_LED 16//D0
+
+const char *ssid = "Router666";
+const char *password = "home@2647";
+
+void blink_led(int interval, int count) {
+	for (int i = 0; i < count; i++) {
+		builtinledSetStatus(true);
+		delay(interval);
+		builtinledSetStatus(false);
+		delay(interval);
+	}
+}
+
+void setup() {
+	Serial.begin(115200);
+	Serial.setRxBufferSize(32);
+	Serial.setDebugOutput(false);
+
+	pinMode(2, OUTPUT);
+  pinMode(16, OUTPUT);
+	WiFi.mode(WIFI_STA);
+	WiFi.persistent(false);
+	WiFi.disconnect(false);
+	WiFi.setAutoReconnect(true);
+	WiFi.begin(ssid, password);
+
+	printf("\n");
+	printf("SketchSize: %d B\n", ESP.getSketchSize());
+	printf("FreeSketchSpace: %d B\n", ESP.getFreeSketchSpace());
+	printf("FlashChipSize: %d B\n", ESP.getFlashChipSize());
+	printf("FlashChipRealSize: %d B\n", ESP.getFlashChipRealSize());
+	printf("FlashChipSpeed: %d\n", ESP.getFlashChipSpeed());
+	printf("SdkVersion: %s\n", ESP.getSdkVersion());
+	printf("FullVersion: %s\n", ESP.getFullVersion().c_str());
+	printf("CpuFreq: %dMHz\n", ESP.getCpuFreqMHz());
+	printf("FreeHeap: %d B\n", ESP.getFreeHeap());
+	printf("ResetInfo: %s\n", ESP.getResetInfo().c_str());
+	printf("ResetReason: %s\n", ESP.getResetReason().c_str());
+	DEBUG_HEAP();
+	homekit_setup();
+	DEBUG_HEAP();
+	blink_led(200, 3);
+}
+
+void loop() {
+	homekit_loop();
+	delay(5);
+}
+
+void builtinledSetStatus(bool on) {
+	digitalWrite(PIN_LED, on ? LOW : HIGH);
+}
+
+//==============================
+// Homekit setup and loop
+//==============================
+
+extern "C" homekit_server_config_t config;
+extern "C" homekit_characteristic_t name;
+extern "C" void led_toggle();
+extern "C" void accessory_init();
+
+uint32_t next_heap_millis = 0;
+
+void homekit_setup() {
+	accessory_init();
+	uint8_t mac[WL_MAC_ADDR_LENGTH];
+	WiFi.macAddress(mac);
+	int name_len = snprintf(NULL, 0, "%s_%02X%02X%02X", name.value.string_value, mac[3], mac[4], mac[5]);
+	char *name_value = (char*)malloc(name_len + 1);
+	snprintf(name_value, name_len + 1, "%s_%02X%02X%02X", name.value.string_value, mac[3], mac[4], mac[5]);
+	name.value = HOMEKIT_STRING_CPP(name_value);
+
+	arduino_homekit_setup(&config);
+ 
+}
+
+void homekit_loop() {
+	arduino_homekit_loop();
+	uint32_t time = millis();
+	if (time > next_heap_millis) {
+		INFO("heap: %d, sockets: %d", ESP.getFreeHeap(), arduino_homekit_connected_clients_count());
+		next_heap_millis = time + 5000;
+	}
+}
